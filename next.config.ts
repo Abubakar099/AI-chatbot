4e7b1f61eae58b6a3190bd0bb4db613d34a0b4f7
Abubakar Ghias <abubakarghias@gmail.com>
2026-06-10 18:53:30 +0500

    update

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
 // next.config.ts
 import type { NextConfig } from "next";
 
+
+
 const nextConfig: NextConfig = {
   /* In Next.js 16.2, 'allowedDevOrigins' is a top-level property.
      It is no longer under 'experimental'.
   */
+     
   allowedDevOrigins: [
     'http://localhost:3000',
-    'http://192.168.10.8:3000' // Add your specific local IP here
+    'http://192.168.100.82' // Add your specific local IP here
   ],
 
   // Other standard configs
